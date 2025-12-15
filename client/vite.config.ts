ebdf9f72b5a36075a2ae98b3740e4487aa5f834b
sirk007 <147782045+sirk007@users.noreply.github.com>
2025-12-15 08:05:11 +0000

    Initial project setup (client + server)
    - Vite + React + Typescript Client
    - Material UI installed
    - Configured IPv4 instead of IPv6 - Firewall blocking port on IPv6 through Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Force IPv4 instead of IPv6 - Firewall blocking it
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173,        // you can change this if needed
+    strictPort: false // allows fallback like 5174 if 5173 is taken
+  }
 })
